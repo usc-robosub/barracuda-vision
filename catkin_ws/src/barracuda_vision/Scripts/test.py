8f2539c1197c18158b18be9717a99a1f44bfec52
Takatoshi Soeda <toshi.scp11@gmail.com>
2025-03-06 20:55:06 -0800

    test update

--- catkin_ws/src/barracuda_vision/Scripts/test.py
+++ catkin_ws/src/barracuda_vision/Scripts/test.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Int8
 from darknet_ros_msgs.msg import BoundingBoxes
 from inference_sdk import InferenceHTTPClient
+from cv_bridge import CvBridge
 
 client = InferenceHTTPClient(
     api_url="http://localhost:9001", # use local inference server
@@ -14,11 +15,12 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + " I heard an image")
     # Process the data and publish the result
     # Assuming data contains the image data
+    image = CvBridge().imgmsg_to_cv2(data, "bgr8")
     result = client.run_workflow(
         workspace_name="roboflow-docs",
         workflow_id="model-comparison",
         images={
-            "image": data
+            "image": image
         },
         parameters={
             "model1": "yolov8n-640",
